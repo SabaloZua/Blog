b0fd68fe45118f8f3bbc7c94efbb8028f06c6758
AstronautaSze <sabalozua@outlook.pt>
2024-04-07 22:46:38 +0100

    O primeiro Commit do blog

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ app.use(passport.session());
 // metodo para messagens temporarias 
 app.use(flash());
 
+app.use(function(req, res, next){
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+   });
 //variaveis globais
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
