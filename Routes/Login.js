1614e5cb059e34c4e877a3ef5c5da8bef9bcc838
AstronautaSze <sabalozua@outlook.pt>
2024-04-08 13:56:37 +0100

    correção de erros

--- Routes/Login.js
+++ Routes/Login.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const Controller=require('../Controllers/Login-Controller');
-const db=require('../Modules/db');
+
 // Rota que busca a pagina de Login
 router.get("/",Controller.PaginaLogin);
  
@@ -10,14 +10,8 @@ router.post('/',Controller.Iniciar_Sessao);
 
 router.get('/TerminarSessao',(req,res)=>{
     req.logOut((err)=>{
-        console.log(err);
+       console.log(err)
     });
-    const sessionId = req.sessionID;
-        db.query('DELETE FROM session WHERE id = $1', [sessionId], function(err) {
-           if (err) {
-             console.error('Erro ao remover a sessão do banco de dados:', err);
-           }
-        });
     req.flash('success_msg',"Sessão terminado com Sucesso.")
     res.redirect('/');
 });
